a2cd75d3435aa19f7d2f617c491d6c9cd3ac04ec
Hun-Se <kim72840@gmail.com>
2024-10-05 13:45:45 +0900

    [fix] 프록시 에러 해결
    mybatis 오류 아직 해결 못함

--- backend/src/main/java/com/lx/finalproject/dao/ExampleDaoImpl.java
+++ backend/src/main/java/com/lx/finalproject/dao/ExampleDaoImpl.java
@@ -2,13 +2,21 @@ package com.lx.finalproject.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
 import com.lx.finalproject.vo.ExampleVo;
 
+@Repository
 public class ExampleDaoImpl implements ExampleDao {
+	
+	@Autowired
+	private SqlSession sqlsession;
 	
 	@Override
 	public List<ExampleVo> getAllExamples() {
-		return null;
+		return sqlsession.selectList("getAllExamples");
 	}
 
 	@Override
